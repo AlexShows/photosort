dcc1b978a83e1dc49712768f63e05449ad3d431b
AlexShows <tashows@gmail.com>
2016-08-28 20:11:28 -0500

    Added recursive directory creation

--- sortpics.js
+++ sortpics.js
@@ -9,28 +9,56 @@ function checkFileInfo(filename) {
       new img({ image : filename }, function(err, exif) {
         if(!err) {
           
+          var fileCreatedDate = exif.exif.CreateDate.toString();
+          
           console.log(filename + 
             ' was created on ' + 
-            exif.exif.CreateDate.toString());
+            fileCreatedDate);
           
-          fs.mkdir(exif.exif.CreateDate.toString(),
+          // Extract year, month, day from CreateDate
+          var year = fileCreatedDate.substring(0,4);
+          var month = fileCreatedDate.substring(5,7);
+          var day = fileCreatedDate.substring(8,10);
+          
+          console.log('This directory ' + 
+                      year + '/' + 
+                      month + '/' + 
+                      day);
+          
+          fs.mkdir(year,
                   null,
                   function(err) {
                     
                     if(err)
                       console.log(err);
-                    else {
-                      // Move the file to a directory
-                      fs.rename(filename,
-                        exif.exif.CreateDate.toString() + '//' +
-                        filename,
-                        function(err) {
-                          if(err)
-                            console.log(err);
+                    
+                    // Create the next directory down
+                    fs.mkdir(year + '/' + month,
+                            null,
+                            function(err) {
+                      
+                      if(err)
+                        console.log(err);
+                        
+                      // Create the next directory down
+                      fs.mkdir(year + '/' + month + '/' + day,
+                              null,
+                              function(err) {
+                        if(err)
+                          console.log(err);
+                        
+                        // Move the file to a directory
+                        fs.rename(filename,
+                          year + '/' + month + '/' + day + '/' +
+                          filename,
+                          function(err) {
+                            if(err)
+                              console.log(err);
                         }); // End rename  
-                    } // End else (no error)
-                  }); // End mkdir
-            
+                        
+                      }); // End mkdir (day)
+                    }); // End mkdir (month)
+                  }); // End mkdir (year)
         } // End if not an error
       }); // End image creation
     } // End if it's not a directory
