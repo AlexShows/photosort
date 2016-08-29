55c5c1e1b5d5bfe7f0e9cbeb19da72744599a878
AlexShows <tashows@gmail.com>
2016-08-28 19:09:56 -0500

    i  Changes to be committed: 	modified:   sortpics.js
     Added move.

--- sortpics.js
+++ sortpics.js
@@ -7,14 +7,26 @@ function checkFileInfo(filename) {
   fs.stat(filename, function(err, stats) {
     if(!stats.isDirectory()) {
       new img({ image : filename }, function(err, exif) {
-        if(!err)
+        if(!err) {
+          
           console.log(filename + 
             ' was created on ' + 
             exif.exif.CreateDate.toString());
-      });
-    }
-  });
-}
+            
+          // Move the file to a directory
+          fs.rename(filename,
+                    exif.exif.CreateDate.toString() + '\\' +
+                    filename,
+                    function(err) {
+                      if(err)
+                        console.log(err);
+                    });
+            
+        } // End if not an error
+      }); // End image creation
+    } // End if it's not a directory
+  }); // End stat on the filename
+} // End checkFileInfo
 
 fs.readdir('./', function(err, dirlisting) {
 
